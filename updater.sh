57a859b8856f727aaf08c351e74ad203f6667ca8
dakokonutboi <48127169+dakokonutboi@users.noreply.github.com>
2019-03-02 15:10:54 +0000

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -5,7 +5,7 @@ $DIALOG --title "OPPAH UPDATER" --clear \
 	--yesno "Would you like to update your oppah?" 10 30
 
 case $? in
-	0)	cd ..; rm -rf oppah; git clone https://github.com/dakokonutboi/oppah.git;;
+	0)	cd ..; rm -rf oppah; rm oppah; git clone https://github.com/dakokonutboi/oppah.git;;
 	1)	echo "You chose not to update oppah.";;
 	255)	echo "Escape pressed";;
 esac
