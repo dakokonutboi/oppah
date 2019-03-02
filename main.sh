41ac6f52a9eda8a558638455a4996cbf47a03773
dakokonutboi <48127169+dakokonutboi@users.noreply.github.com>
2019-03-02 15:39:07 +0000

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+DIALOG=${DIALOG=dialog}
+ fichtemp=`tempfile 2>/dev/null` || fichtemp=/tmp/test$$
+trap "rm -f $fichtemp" 0 1 2 5 15
+$DIALOG --clear --title "Oppah toolbox" \
+	--menu "Choose a tool :" 20 51 4 \
+	 "updater.sh" "Mettez a jour votre version d'oppah" \
+	 "passwordmanager.py" "Un generateur de mots de passe" 2> $fichtemp
+valret=$?
+choix=`cat $fichtemp`
+case $valret in
+ 0)	sh $choix; py $choix;;
+ 1) 	echo "Appuyé sur Annuler.";;
+255) 	echo "Appuyé sur Echap.";;
+esac
