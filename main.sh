1c736d08ee0153646d16cb22f822446b5ababe70
dakokonutboi <48127169+dakokonutboi@users.noreply.github.com>
2019-03-02 15:51:17 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,12 +4,12 @@ DIALOG=${DIALOG=dialog}
 trap "rm -f $fichtemp" 0 1 2 5 15
 $DIALOG --clear --title "Oppah toolbox" \
 	--menu "Choose a tool :" 20 51 4 \
-	 "updater.sh" "Mettez a jour votre version d'oppah" \
-	 "passwordmanager.py" "Un generateur de mots de passe" 2> $fichtemp
+	 "sh updater.sh" "Mettez a jour votre version d'oppah" \
+	 "python passwordmanager.py" "Un generateur de mots de passe" 2> $fichtemp
 valret=$?
 choix=`cat $fichtemp`
 case $valret in
- 0)	sh $choix; python $choix;;
+ 0)	$choix;;
  1) 	echo "Appuyé sur Annuler.";;
 255) 	echo "Appuyé sur Echap.";;
 esac
