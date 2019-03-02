1bbabb62b04b94a4d3b9aa1460dd084710856a6e
dakokonutboi <48127169+dakokonutboi@users.noreply.github.com>
2019-03-02 15:56:10 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 DIALOG=${DIALOG=dialog}
+VERSION=$(head -n 1 version.txt)
  fichtemp=`tempfile 2>/dev/null` || fichtemp=/tmp/test$$
 trap "rm -f $fichtemp" 0 1 2 5 15
-$DIALOG --clear --title "Oppah toolbox" \
+$DIALOG --clear --title "Oppah toolbox - $version" \
 	--menu "Choose a tool :" 20 51 4 \
 	 "sh updater.sh" "Mettez a jour votre version d'oppah" \
 	 "python passwordmanager.py" "Un generateur de mots de passe" 2> $fichtemp
